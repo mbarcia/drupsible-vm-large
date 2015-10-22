fae8084707c3c41db4001852514b852a73f63292
Mariano Barcia <mariano.barcia@gmail.com>
2015-10-22 19:32:00 +0200

    Revert "Better cleanup."
    This reverts commit 5415051565c8c940a31767e39c8e1bd7392760b9.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -9,9 +9,7 @@ rm /var/lib/dhcp/*
 
 # Make sure Udev doesn't block our network
 echo "cleaning up udev rules"
-if [ -f /etc/udev/rules.d/70-persistent-net.rules ]; then
-	rm /etc/udev/rules.d/70-persistent-net.rules
-fi
+rm /etc/udev/rules.d/70-persistent-net.rules
 mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
